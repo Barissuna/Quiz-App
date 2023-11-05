1df14e97b0f04fe51bf6470a2987641b94f62fae
Barış Suna <barsuna02@hotmail.com>
2023-11-05 21:09:02 +0300

    handling exceptions

--- src/main/java/com/barissuna/quizapp/service/QuestionService.java
+++ src/main/java/com/barissuna/quizapp/service/QuestionService.java
@@ -3,11 +3,15 @@ package com.barissuna.quizapp.service;
 import com.barissuna.quizapp.Question;
 import com.barissuna.quizapp.dao.QuestionDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,18 +19,36 @@ public class QuestionService {
 
     @Autowired
     QuestionDao questionDao;
-    public List<Question> getAllQuestions() {
-        return questionDao.findAll();
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try{
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
     }
 
 
-    public List<Question> getQuestionsByCategory(String category) {
-        return questionDao.findByCategory(category);
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try{
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(),HttpStatus.BAD_REQUEST);
+
     }
 
 
-    public String addQuestion(Question question) {
-        questionDao.save(question);
-        return "Success";
+    public ResponseEntity<String> addQuestion(Question question) {
+        try{
+            questionDao.save(question);
+            return new ResponseEntity<>("Success",HttpStatus.CREATED);
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>("Success",HttpStatus.BAD_REQUEST);
+
     }
 }
