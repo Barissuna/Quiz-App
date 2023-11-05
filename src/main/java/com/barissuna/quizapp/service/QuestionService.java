8b13a236c4337b7413b7a6f5933b77727b32521e
Barış Suna <barsuna02@hotmail.com>
2023-11-05 15:06:16 +0300

    Dao added

--- src/main/java/com/barissuna/quizapp/service/QuestionService.java
+++ src/main/java/com/barissuna/quizapp/service/QuestionService.java
@@ -1,12 +1,18 @@
 package com.barissuna.quizapp.service;
 
 import com.barissuna.quizapp.Question;
+import com.barissuna.quizapp.dao.QuestionDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
 public class QuestionService {
+
+    @Autowired
+    QuestionDao questionDao;
     public List<Question> getAllQuestions() {
+        return questionDao.findAll();
     }
 }
