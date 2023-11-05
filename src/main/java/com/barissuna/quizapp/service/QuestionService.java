68ee92c80995f96abab2ad5bbc25540e692e2369
Barış Suna <barsuna02@hotmail.com>
2023-11-05 14:47:41 +0300

    configuration of Controller, Service and Entity

---
+++ src/main/java/com/barissuna/quizapp/service/QuestionService.java
@@ -0,0 +1,12 @@
+package com.barissuna.quizapp.service;
+
+import com.barissuna.quizapp.Question;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class QuestionService {
+    public List<Question> getAllQuestions() {
+    }
+}
